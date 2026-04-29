4a011cb8d544d98badb4d1c8d8c07ddf51a0c542
Eddie.Vels <eddie@vels.online>
2026-04-29 16:46:03 +0200

    .

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,9 +11,9 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
-CORS_ALLOWED_ORIGINS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
